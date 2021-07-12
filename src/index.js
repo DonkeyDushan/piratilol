54cb94f0c04b8bd2f8324774c7c6a71d90798ec1
Natálie Sedláková <natsedlakova@gmail.com>
2021-07-13 01:38:10 +0200

    refact

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import { claims, generators } from "./data.js";
 
-const unrolledGenerators = generators.flatMap(({ url, weight }) => Array(weight).fill(url));
+const splitText = (text, maxLineLength) => {
+  const result = [];
+  while (text.length > maxLineLength) {
+    let pos = text.substring(0, maxLineLength).lastIndexOf(" ");
+    pos = pos <= 0 ? maxLineLength : pos;
+    result.push(text.substring(0, pos));
+    let i = text.indexOf(" ", pos) + 1;
+    if (i < pos || i > pos + maxLineLength) i = pos;
+    text = text.substring(i);
+  }
+  result.push(text);
+  return result;
+};
+
+const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const canvas = document.getElementById("picture");
 const ctx = canvas.getContext("2d");
@@ -13,24 +27,7 @@ const initFont = async () => {
   ctx.font = `${fontSize}px 'Bebas Neue'`;
 };
 
-const splitter = (str, l) => {
-  const strs = [];
-  while (str.length > l) {
-    let pos = str.substring(0, l).lastIndexOf(" ");
-    pos = pos <= 0 ? l : pos;
-    strs.push(str.substring(0, pos));
-    let i = str.indexOf(" ", pos) + 1;
-    if (i < pos || i > pos + l) i = pos;
-    str = str.substring(i);
-  }
-  strs.push(str);
-  return strs;
-};
-
-const rand = (n) => Math.floor(Math.random() * n);
-
-const getText = () => claims[rand(claims.length)];
-const getGenerator = () => unrolledGenerators[rand(unrolledGenerators.length)];
+const unrolledGenerators = generators.flatMap(({ url, weight }) => Array(weight).fill(url));
 
 const initImage = async (customText) => {
   const loadLogo = () => {
@@ -46,27 +43,25 @@ const initImage = async (customText) => {
     });
   };
 
-  const imageData = await fetch(getGenerator());
+  const imageData = await fetch(pickRandom(unrolledGenerators));
   const image = new Image();
   image.crossOrigin = "anonymous";
   image.src = imageData.url;
   image.addEventListener("load", () => {
     ctx.drawImage(image, 0, 0);
     loadLogo();
-    let i = 0;
-    const unsplitText = customText ?? getText();
-    const text = splitter(unsplitText, 20).reverse();
-    text.forEach((line) => {
+    const unsplitText = customText ?? pickRandom(claims);
+    const text = splitText(unsplitText, 20).reverse();
+    text.forEach((line, index) => {
       const x = 30;
       const y = 685;
       const padding = 15;
       const lineHeight = padding + fontSize;
       ctx.fillStyle = "#f9dc4d";
-      ctx.fillRect(x, y - (i * lineHeight), ctx.measureText(line).width + 2 * padding, lineHeight);
+      ctx.fillRect(x, y - (index * lineHeight), ctx.measureText(line).width + 2 * padding, lineHeight);
       ctx.textBaseline = "top";
       ctx.fillStyle = "black";
-      ctx.fillText(line, x + padding, y + padding - (i * lineHeight));
-      i += 1;
+      ctx.fillText(line, x + padding, y + padding - (index * lineHeight));
     });
   });
 };
@@ -74,11 +69,11 @@ const initImage = async (customText) => {
 const buttonRandom = document.getElementById("randomize");
 buttonRandom.onclick = () => initImage();
 
-const customInput = document.getElementById("customText");
+const inputCustom = document.getElementById("customText");
 const buttonCustom = document.getElementById("submitCustomText");
 buttonCustom.onclick = () => {
-  if (customInput.value) {
-    initImage(customInput.value);
+  if (inputCustom.value) {
+    initImage(inputCustom.value);
   }
 };
 
