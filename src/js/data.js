f3a9e27cebbffd6ac3c34d5c534ffdf8dcfc73fc
Michal Zlatkovský <michal@zlatkovsky.cz>
2025-03-26 19:09:55 +0100

    comment out unsplash

--- src/js/data.js
+++ src/js/data.js
@@ -29,10 +29,12 @@ export const claims = [
 
 export const generators = [
   { url: "https://picsum.photos/800", weight: 5 },
+  /*
   { url: "https://source.unsplash.com/800x800", weight: 10 },
   { url: "https://source.unsplash.com/800x800/?communism", weight: 3 },
   { url: "https://source.unsplash.com/800x800/?cat", weight: 1 },
   { url: "https://source.unsplash.com/800x800/?puppy", weight: 1 },
   { url: "https://source.unsplash.com/800x800/?chicken", weight: 1 },
   { url: "https://source.unsplash.com/800x800/?angry", weight: 3 },
+   */
 ];
